540a5163585748ce1c97170e3b525a76ee486395
mcsandovall <rasec_oiram@live.com.mx>
2022-03-01 23:59:11 -0600

    vertex and frag file

--- A3/resources/normal_frag.glsl
+++ A3/resources/normal_frag.glsl
@@ -1,30 +1,8 @@
 #version 120
-uniform vec3 lightPos;
-uniform vec3 ka;
-uniform vec3 kd;
-uniform vec3 ks;
-uniform float s;
 
-//varying vec3 color; // passed from the vertex shader
-varying vec3 cNor;
-varying vec3 cPos;
+varying vec3 color; // passed from the vertex shader
 
 void main()
 {
-    // variable definitions
-    vec3 n, l, ca, cd, cs, c, h, e, r;
-    
-    // compute the normals
-    n = normalize(cNor);
-    l = normalize(lightPos - cPos);
-    e = normalize(-cPos); // e vector
-    
-    h = normalize(l + e);
-    
-    //perform lighting computation
-    ca = ka; // final ambient color
-    cd = kd * max(0.0,dot(l,n));
-    cs = ks * pow(max(0,dot(h,n)), s);
-    c = ca + cd + cs;
-	gl_FragColor = vec4(c, 1.0);
+    gl_FragColor = vec4(color.r, color.g, color.b, 1.0);
 }
