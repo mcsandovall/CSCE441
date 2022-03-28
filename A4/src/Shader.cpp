dbfe87e3d4305197aed01f90511af0e25ff591db
mcsandovall <rasec_oiram@live.com.mx>
2022-03-28 16:47:34 -0500

    moved Shader to external file

---
+++ A4/src/Shader.cpp
@@ -0,0 +1,57 @@
+#include "Shader.h"
+#include "Program.h"
+#include <memory>
+#include "MatrixStack.h"
+
+using namespace std;
+
+#define RESOURCE_DIR "../../resources/"
+
+// Shader class function implementation
+Shader::Shader(string vertex_file, string frag_file){
+    // move this to another func for the uniforms
+    program = make_shared<Program>();
+    program->setShaderNames(RESOURCE_DIR + vertex_file, RESOURCE_DIR + frag_file);
+    program->setVerbose(true);
+    program->init();
+    program->addAttribute("aPos");
+    program->addAttribute("aNor");
+    program->addUniform("MV");
+    program->addUniform("P");
+    program->addUniform("MVit"); // add the uniform
+    program->addUniform("lightPos");
+    program->addUniform("ka");
+    program->addUniform("kd");
+    program->addUniform("ks");
+    program->addUniform("s");
+    program->setVerbose(false);
+}
+
+void Shader::bind(shared_ptr<MatrixStack> P, shared_ptr<MatrixStack> MV, Material M, bool twoD = false){
+    program->bind();
+    // make the MVit
+    if(twoD){
+//        int width, height;
+//        glfwGetWindowSize(window, &width, &height);
+        float aspect = 1;
+        glUniformMatrix4fv(program->getUniform("P"), 1, GL_FALSE, glm::value_ptr(glm::ortho(-aspect, aspect, -1.0f, 1.0f, 0.1f, 1000.0f)));
+        glUniform3f(program->getUniform("lightPos"), 0.0f, 0.0f, 100.0f);
+    }else{
+        glUniformMatrix4fv(program->getUniform("P"), 1, GL_FALSE, glm::value_ptr(P->topMatrix()));
+        // make the light position
+        glm::vec3 lightPos(8.0,10.0,1.0);
+        lightPos = glm::vec3(MV->topMatrix() * glm::vec4(lightPos,1.0));
+        glUniform3f(program->getUniform("lightPos"), lightPos.x, lightPos.y, lightPos.z);
+    }
+    glm::mat4 MVit = glm::inverse(glm::transpose(MV->topMatrix()));
+    glUniformMatrix4fv(program->getUniform("MV"), 1, GL_FALSE, glm::value_ptr(MV->topMatrix()));
+    glUniformMatrix4fv(program->getUniform("MVit"), 1, GL_FALSE, glm::value_ptr(MVit));
+    glUniform3f(program->getUniform("ka"), M.ka.x,M.ka.y,M.ka.z);
+    glUniform3f(program->getUniform("kd"), M.kd.x,M.kd.y,M.kd.z);
+    glUniform3f(program->getUniform("ks"), M.ks.x,M.ks.y,M.ks.z);
+    glUniform1f(program->getUniform("s"),  M.s);
+}
+
+void Shader::program_unbind(){
+    program->unbind();
+}
